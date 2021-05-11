bf2fbf714d51d09d4675dde4112ca3337935ec26
Frank Wang <frankwang95174@gmail.com>
2021-05-12 01:43:25 +0800

    add button to connect metamask

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,32 @@
 <template>
   <v-app>
     <v-app-bar app color="dark" dark>
-      <v-spacer></v-spacer>
-      <div class="d-flex align-center">
-        <router-link :to="{ name: 'Home' }">
-          <v-btn text>
-            <span class="mr-2">Pinture</span>
+      <v-row align="center">
+        <v-col cols="4" class="tex-left">
+          <h3>Your Address</h3>
+          <h5>
+            {{
+              currentAccount === ""
+                ? "Please connect to metamask.."
+                : currentAccount
+            }}
+          </h5>
+        </v-col>
+        <v-col cols="4" class="text-center">
+          <router-link :to="{ name: 'Home' }">
+            <v-btn text>
+              <span class="mr-2">Pinture</span>
+            </v-btn>
+          </router-link>
+        </v-col>
+        <v-col cols="4" class="text-right">
+          <v-btn text @click="connect">
+            <span class="mr-2">{{
+              currentAccount === "" ? "Connect 🦊" : "Connected 🦊"
+            }}</span>
           </v-btn>
-        </router-link>
-      </div>
-      <v-spacer></v-spacer>
+        </v-col>
+      </v-row>
     </v-app-bar>
     <v-main>
       <router-view />
@@ -18,12 +35,69 @@
 </template>
 
 <script>
+import detectEthereumProvider from "@metamask/detect-provider";
 export default {
   name: "App",
+  data() {
+    return {
+      currentAccount: ""
+    };
+  },
+  methods: {
+    startApp(provider) {
+      // If the provider returned by detectEthereumProvider is not the same as
+      // window.ethereum, something is overwriting it, perhaps another wallet.
+      if (provider !== window.ethereum) {
+        console.error("Do you have multiple wallets installed?");
+      }
+      // Access the decentralized web!
+    },
+    handleChainChanged(_chainId) {
+      // We recommend reloading the page, unless you must do otherwise
+      window.location.reload();
+    },
+    // For now, 'eth_accounts' will continue to always return an array
+    handleAccountsChanged(accounts) {
+      if (accounts.length === 0) {
+        // MetaMask is locked or the user has not connected any accounts
+        console.log("Please connect to MetaMask.");
+      } else if (accounts[0] !== this.currentAccount) {
+        this.currentAccount = accounts[0];
+        // Do any other work!
+      }
+    },
+    connect() {
+      ethereum
+        .request({ method: "eth_requestAccounts" })
+        .then(this.handleAccountsChanged)
+        .catch((err) => {
+          if (err.code === 4001) {
+            // EIP-1193 userRejectedRequest error
+            // If this happens, the user rejected the connection request.
+            console.log("Please connect to MetaMask.");
+          } else {
+            console.error(err);
+          }
+        });
+    }
+  },
+  async mounted() {
+    const provider = await detectEthereumProvider();
+    if (provider) {
+      // From now on, this should always be true:
+      // provider === window.ethereum
+      this.startApp(provider); // initialize your app
+    } else {
+      console.log("Please install MetaMask!");
+    }
 
-  data: () => ({
-    //
-  })
+    ethereum.on("chainChanged", this.handleChainChanged);
+
+    // Note that this event is emitted on page load.
+    // If the array of accounts is non-empty, you're already
+    // connected.
+    ethereum.on("accountsChanged", this.handleAccountsChanged);
+  }
 };
 </script>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -45,7 +45,6 @@
 
 <script>
 // @ is an alias to /src
-import detectEthereumProvider from "@metamask/detect-provider";
 import { ethers } from "ethers";
 import PhotoTokenJson from "../assets/contracts/PhotoToken.json";
 
@@ -61,46 +60,6 @@ export default {
     };
   },
   methods: {
-    startApp(provider) {
-      // If the provider returned by detectEthereumProvider is not the same as
-      // window.ethereum, something is overwriting it, perhaps another wallet.
-      if (provider !== window.ethereum) {
-        console.error("Do you have multiple wallets installed?");
-      }
-      // Access the decentralized web!
-    },
-    handleChainChanged(_chainId) {
-      // We recommend reloading the page, unless you must do otherwise
-      window.location.reload();
-    },
-    // For now, 'eth_accounts' will continue to always return an array
-    handleAccountsChanged(accounts) {
-      if (accounts.length === 0) {
-        // MetaMask is locked or the user has not connected any accounts
-        console.log("Please connect to MetaMask.");
-      } else if (accounts[0] !== this.currentAccount) {
-        this.currentAccount = accounts[0];
-        // Do any other work!
-      }
-    },
-    // While you are awaiting the call to eth_requestAccounts, you should disable
-    // any buttons the user can click to initiate the request.
-    // MetaMask will reject any additional requests while the first is still
-    // pending.
-    connect() {
-      ethereum
-        .request({ method: "eth_requestAccounts" })
-        .then(handleAccountsChanged)
-        .catch((err) => {
-          if (err.code === 4001) {
-            // EIP-1193 userRejectedRequest error
-            // If this happens, the user rejected the connection request.
-            console.log("Please connect to MetaMask.");
-          } else {
-            console.error(err);
-          }
-        });
-    },
     sendETH() {
       const vm = this;
       ethereum.request({
@@ -190,47 +149,7 @@ export default {
       console.log(await PhotoTokenWithSigner.balanceOf(this.currentAccount));
     }
   },
-  async mounted() {
-    const provider = await detectEthereumProvider();
-    if (provider) {
-      // From now on, this should always be true:
-      // provider === window.ethereum
-      this.startApp(provider); // initialize your app
-    } else {
-      console.log("Please install MetaMask!");
-    }
-    /**********************************************************/
-    /* Handle chain (network) and chainChanged (per EIP-1193) */
-    /**********************************************************/
-    const chainId = await ethereum.request({ method: "eth_chainId" });
-    // this.handleChainChanged(chainId);
-    ethereum.on("chainChanged", this.handleChainChanged);
-    /***********************************************************/
-    /* Handle user accounts and accountsChanged (per EIP-1193) */
-    /***********************************************************/
-    ethereum
-      .request({ method: "eth_requestAccounts" })
-      .then(this.handleAccountsChanged)
-      .catch((err) => {
-        // Some unexpected error.
-        // For backwards compatibility reasons, if no accounts are available,
-        // eth_accounts will return an empty array.
-        console.error(err);
-      });
-    // Note that this event is emitted on page load.
-    // If the array of accounts is non-empty, you're already
-    // connected.
-    ethereum.on("accountsChanged", this.handleAccountsChanged);
-    /*********************************************/
-    /* Access the user's accounts (per EIP-1102) */
-    /*********************************************/
-    // You should only attempt to request the user's accounts in response to user
-    // interaction, such as a button click.
-    // Otherwise, you popup-spam the user like it's 1999.
-    // If you fail to retrieve the user's account(s), you should encourage the user
-    // to initiate the attempt.
-    document.getElementById("connectButton", this.connect);
-  },
+  async mounted() {},
   async created() {}
 };
 </script>
