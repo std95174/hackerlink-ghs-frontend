a9e6bfd60a73dd0b86485a4a6601169315557a74
Frank Wang <frankwang95174@gmail.com>
2021-05-13 00:40:22 +0800

    add two NFT components

---
+++ src/components/LicenseNFT.vue
@@ -0,0 +1,135 @@
+<template>
+  <v-dialog v-model="dialog">
+    <v-card>
+      <v-card-title class="headline">Make Your License NFT</v-card-title>
+      <v-card-text>
+        <div>
+          <v-stepper v-model="e1">
+            <v-stepper-header>
+              <v-stepper-step :complete="e1 > 1" step="1">
+                License
+              </v-stepper-step>
+
+              <v-divider></v-divider>
+
+              <v-stepper-step :complete="e1 > 2" step="2"> NFT </v-stepper-step>
+
+              <v-divider></v-divider>
+
+              <v-stepper-step step="3"> Done! </v-stepper-step>
+            </v-stepper-header>
+
+            <v-stepper-items>
+              <v-stepper-content step="1">
+                <v-card class="mb-12" color=" lighten-1" height="200px">
+                  <v-card-title>Config Your License</v-card-title>
+                </v-card>
+                <div class="text-right">
+                  <v-btn color="primary" @click="e1 = 2"> Continue </v-btn>
+                </div>
+              </v-stepper-content>
+
+              <v-stepper-content step="2">
+                <v-card class="mb-12" color=" lighten-1" height="200px">
+                  <v-card-title> Make a NFT </v-card-title>
+                </v-card>
+                <div class="text-right">
+                  <v-btn text @click="e1 = 1" class="mr-3">
+                    <v-icon>mdi-arrow-left</v-icon>Previous
+                  </v-btn>
+                  <v-btn color="primary" @click="e1 = 3"> Continue </v-btn>
+                </div>
+              </v-stepper-content>
+
+              <v-stepper-content step="3">
+                <v-card class="mb-12" color=" lighten-1" height="200px">
+                  <v-card-title>We1l Done</v-card-title>
+                  <v-card-text>
+                    <div>
+                      It's great that you make you photo a NFT. Now, time to
+                      make a its license NFT and make some money!
+                    </div>
+                    <div>
+                      Your picture on IPFS:
+                      <v-btn
+                        href="http://ipfs.infura.io:5001/api/v0/cat?arg=QmNYwRFaQTYzN1FxvTkjMVPHiTvAw53EmziLjLDa921u23"
+                        target="black"
+                        small
+                        text
+                      >
+                        click me
+                        <v-icon>mdi-image</v-icon>
+                      </v-btn>
+                    </div>
+                    <div>Your NFT ID on Smart Contract: <span>1</span></div>
+                    <div>
+                      Transaction on blockchain:
+                      <v-btn
+                        href="http://ipfs.infura.io:5001/api/v0/cat?arg=QmNYwRFaQTYzN1FxvTkjMVPHiTvAw53EmziLjLDa921u23"
+                        target="black"
+                        small
+                        text
+                      >
+                        click me
+                        <v-icon>mdi-link-lock</v-icon>
+                      </v-btn>
+                    </div>
+                  </v-card-text>
+                </v-card>
+                <div class="text-right">
+                  <v-btn text @click="e1 = 2" class="mr-3">
+                    <v-icon>mdi-arrow-left</v-icon>Previous
+                  </v-btn>
+                  <v-btn
+                    color="primary"
+                    @click="
+                      dialog = false;
+                      el = 1;
+                    "
+                  >
+                    Done and Close
+                  </v-btn>
+                </div>
+              </v-stepper-content>
+            </v-stepper-items>
+          </v-stepper>
+        </div>
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn color="grey darken-1" text @click="close">Close</v-btn>
+        <v-spacer></v-spacer>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  name: "LicenseNFT",
+  props: {
+    value: Boolean
+  },
+  data() {
+    return {
+      e1: 1
+    };
+  },
+  methods: {
+    close() {
+      this.dialog = false;
+      this.$nextTick(() => {});
+    }
+  },
+  computed: {
+    dialog: {
+      get() {
+        return this.value;
+      },
+      set(value) {
+        this.$emit("input", value);
+      }
+    }
+  }
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -11,24 +11,37 @@
         </h3>
       </v-col>
     </v-row>
+    <v-row align="center" justify="center">
+      <v-col class="text-right"
+        ><v-btn @click="photoNFTDialog = true"
+          >📷 Make Your Photo NFT</v-btn
+        ></v-col
+      >
+      <v-col class="text-left"
+        ><v-btn @click="licenseNFTDialog = true"
+          >Make Your License NFT 🔐</v-btn
+        ></v-col
+      >
+    </v-row>
     <v-row class="mr-6 ml-6 pa-6">
       <v-col>
         <span>
-          Licensing photo have always been a pain for image creators. The
-          existing photo authorization services are restricted by the platform
-          itself, and the pricing is controlled by the platform. The creators
-          have no right to set the price for themselves. Moreover, the legal
-          issues of image copyright are complicated, which makes it difficult to
-          authorize on the Internet. The emergence of NFT makes it possible to
-          transfer the ownership of digital assets. Numerous artworks, music,
-          and images have found new life. Creators can free themselves to create
-          without being restricted by physical properties. However, NFTs
-          currently focus on The transfer of ownership, we believe that the
-          right to use also has a lot of room for development, for example, the
-          right to printing and the right to editorial. Traditionally, it has
-          been monopolized by major platforms, and the emergence of NFT can
-          break the deadlock. We hope to maximize the transfer of rights and use
-          blockchain technology to create a open platform for creators.
+          <span class="ml-6"> Licensing</span> photo have always been a pain for
+          image creators. The existing photo authorization services are
+          restricted by the platform itself, and the pricing is controlled by
+          the platform. The creators have no right to set the price for
+          themselves. Moreover, the legal issues of image copyright are
+          complicated, which makes it difficult to authorize on the Internet.
+          The emergence of NFT makes it possible to transfer the ownership of
+          digital assets. Numerous artworks, music, and images have found new
+          life. Creators can free themselves to create without being restricted
+          by physical properties. However, NFTs currently focus on The transfer
+          of ownership, we believe that the right to use also has a lot of room
+          for development, for example, the right to printing and the right to
+          editorial. Traditionally, it has been monopolized by major platforms,
+          and the emergence of NFT can break the deadlock. We hope to maximize
+          the transfer of rights and use blockchain technology to create a open
+          platform for creators.
         </span>
       </v-col>
     </v-row>
@@ -40,6 +53,8 @@
     <v-btn color="red" @click="getBalance"
       >Frank get balance from PhotoToken contract</v-btn
     > -->
+    <photo-nft v-model="photoNFTDialog"></photo-nft>
+    <license-nft v-model="licenseNFTDialog"></license-nft>
   </v-parallax>
 </template>
 
@@ -47,16 +62,20 @@
 // @ is an alias to /src
 import { ethers } from "ethers";
 import PhotoTokenJson from "../assets/contracts/PhotoToken.json";
+import PhotoNFT from "@/components/PhotoNFT.vue";
+import LicenseNFT from "@/components/LicenseNFT.vue";
 
-import HelloWorld from "@/components/HelloWorld.vue";
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    "photo-nft": PhotoNFT,
+    "license-nft": LicenseNFT
   },
   data() {
     return {
-      currentAccount: "unconnected yet"
+      currentAccount: "unconnected yet",
+      photoNFTDialog: false,
+      licenseNFTDialog: false
     };
   },
   methods: {
