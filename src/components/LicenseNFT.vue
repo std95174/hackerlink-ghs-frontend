13226b3a943e95a180ac8c500541b7743181bc4e
Frank Wang <frankwang95174@gmail.com>
2021-05-15 20:06:24 +0800

    add form validate

--- src/components/LicenseNFT.vue
+++ src/components/LicenseNFT.vue
@@ -49,8 +49,13 @@
                           </v-row>
                         </v-container>
                       </template>
-                      <template v-slot:[`item.actions`]="{}">
-                        <v-btn color="primary" @click="e1 = 2">
+                      <template v-slot:[`item.actions`]="{ item }">
+                        <v-btn
+                          color="primary"
+                          @click="
+                            choosePictureToken(item.tokenId, item.tokenUri)
+                          "
+                        >
                           Continue
                         </v-btn>
                       </template>
@@ -60,14 +65,108 @@
               </v-stepper-content>
 
               <v-stepper-content step="2">
-                <v-card class="mb-12" color=" lighten-1" height="200px">
+                <v-card class="mb-12" color=" lighten-1">
                   <v-card-title> Config Your License </v-card-title>
+                  <v-card-text>
+                    <form>
+                      <v-text-field
+                        v-model="to"
+                        :error-messages="toErrors"
+                        :counter="42"
+                        label="Address You Want To Authorize"
+                        required
+                        @input="$v.to.$touch()"
+                        @blur="$v.to.$touch()"
+                      ></v-text-field>
+                      <v-text-field
+                        v-model="tokenId"
+                        :error-messages="tokenIdErrors"
+                        label="Picture Token Id"
+                        disabled
+                        required
+                      ></v-text-field>
+                      <v-text-field
+                        v-model="tokenUri"
+                        :error-messages="tokenUriErrors"
+                        label="Picture Token URI"
+                        disabled
+                        required
+                      ></v-text-field>
+                      <v-menu
+                        v-model="startTimeMenu"
+                        :close-on-content-click="false"
+                        :nudge-right="40"
+                        transition="scale-transition"
+                        offset-y
+                        min-width="auto"
+                      >
+                        <template v-slot:activator="{ on, attrs }">
+                          <v-text-field
+                            v-model="startTime"
+                            :error-messages="startTimeErrors"
+                            label="Click to Pick Start Time"
+                            readonly
+                            v-bind="attrs"
+                            v-on="on"
+                            @change="$v.startTime.$touch()"
+                            @blur="$v.startTime.$touch()"
+                          ></v-text-field>
+                        </template>
+                        <v-date-picker
+                          v-model="startTime"
+                          @input="startTimeMenu = false"
+                        ></v-date-picker>
+                      </v-menu>
+                      <v-menu
+                        v-model="endTimeMenu"
+                        :close-on-content-click="false"
+                        :nudge-right="40"
+                        transition="scale-transition"
+                        offset-y
+                        min-width="auto"
+                      >
+                        <template v-slot:activator="{ on, attrs }">
+                          <v-text-field
+                            v-model="endTime"
+                            :error-messages="endTimeErrors"
+                            label="Click to Pick End Time"
+                            readonly
+                            v-bind="attrs"
+                            v-on="on"
+                            @change="$v.endTime.$touch()"
+                            @blur="$v.endTime.$touch()"
+                          ></v-text-field>
+                        </template>
+                        <v-date-picker
+                          v-model="endTime"
+                          @input="endTimeMenu = false"
+                        ></v-date-picker>
+                      </v-menu>
+                      <v-text-field
+                        v-model="quantity"
+                        :error-messages="quantityErrors"
+                        type="number"
+                        label="Quantity"
+                        required
+                        @change="$v.quantity.$touch()"
+                        @blur="$v.quantity.$touch()"
+                      ></v-text-field>
+                      <v-checkbox
+                        v-model="checkbox"
+                        :error-messages="checkboxErrors"
+                        label="Are you sure?"
+                        required
+                        @change="$v.checkbox.$touch()"
+                        @blur="$v.checkbox.$touch()"
+                      ></v-checkbox>
+                    </form>
+                  </v-card-text>
                 </v-card>
                 <div class="text-right">
                   <v-btn text @click="e1 = 1" class="mr-3">
                     <v-icon>mdi-arrow-left</v-icon>Previous
                   </v-btn>
-                  <v-btn color="primary" @click="e1 = 3"> Continue </v-btn>
+                  <v-btn color="primary" @click="submit"> Mint </v-btn>
                 </div>
               </v-stepper-content>
 
@@ -138,6 +237,13 @@
 import { mapGetters } from "vuex";
 import { ethers } from "ethers";
 import pictureTokenJson from "../assets/contracts/PictureToken.json";
+import { validationMixin } from "vuelidate";
+import {
+  required,
+  maxLength,
+  minLength,
+  minValue
+} from "vuelidate/lib/validators";
 export default {
   name: "LicenseNFT",
   props: {
@@ -154,7 +260,7 @@ export default {
           width: "10%"
         },
         // { text: "Token ID", align: "center", value: "tokenId", width: "10%" },
-        { text: "Token URI", align: "center", value: "tokenURI", width: "20%" },
+        { text: "Token URI", align: "center", value: "tokenUri", width: "20%" },
         { text: "Photo", align: "center", value: "photo", width: "40%" },
         {
           text: "Actions",
@@ -165,8 +271,34 @@ export default {
         }
       ],
       pintureTokens: [],
-      tableLoading: false
+      tableLoading: false,
+
+      // form
+      to: "",
+      tokenId: "",
+      tokenUri: "",
+      quantity: 0,
+      checkbox: false,
+      startTime: "",
+      endTime: "",
+      startTimeMenu: false,
+      endTimeMenu: false
     };
+  },
+  mixins: [validationMixin],
+
+  validations: {
+    to: { required, maxLength: maxLength(42), minLength: minLength(42) },
+    tokenId: { required },
+    tokenUri: { required },
+    quantity: { required, minValue: minValue(1) },
+    startTime: { required },
+    endTime: { required },
+    checkbox: {
+      checked(val) {
+        return val;
+      }
+    }
   },
   methods: {
     close() {
@@ -205,14 +337,12 @@ export default {
         pictureTokenWithSigner
           .tokenOfOwnerByIndex(this.currentAccount, index)
           .then((tokenId) => {
-            pictureTokenWithSigner.tokenURI(tokenId).then((tokenURI) => {
-              console.log(tokenURI);
-
-              const photo = tokenURI.split("ipfs://");
+            pictureTokenWithSigner.tokenURI(tokenId).then((tokenUri) => {
+              const photo = tokenUri.split("ipfs://");
               vm.pintureTokens.push({
                 index: index,
                 tokenId: tokenId,
-                tokenURI: tokenURI,
+                tokenUri: tokenUri,
                 photo: photo[1]
               });
             });
@@ -220,6 +350,22 @@ export default {
               vm.tableLoading = false;
             }
           });
+      }
+    },
+    choosePictureToken(tokenId, tokenUri) {
+      this.tokenId = tokenId._hex;
+      this.tokenUri = tokenUri;
+      this.e1 = 2;
+    },
+    // vuelidate
+    submit() {
+      console.log("submit!");
+      this.$v.$touch();
+      if (this.$v.$invalid) {
+        this.submitStatus = "ERROR";
+      } else {
+        // do your submit logic here
+        this.e1 = 3;
       }
     }
   },
@@ -232,6 +378,53 @@ export default {
       set(value) {
         this.$emit("input", value);
       }
+    },
+    checkboxErrors() {
+      const errors = [];
+      if (!this.$v.checkbox.$dirty) return errors;
+      !this.$v.checkbox.checked && errors.push("You must agree to continue!");
+      return errors;
+    },
+    toErrors() {
+      const errors = [];
+      if (!this.$v.to.$dirty) return errors;
+      !this.$v.to.maxLength &&
+        errors.push("Address must be 42 characters and with 0x prefix");
+      !this.$v.to.minLength &&
+        errors.push("Address must be 42 characters and with 0x prefix");
+      !this.$v.to.required && errors.push("Address is required.");
+      return errors;
+    },
+    tokenIdErrors() {
+      const errors = [];
+      if (!this.$v.tokenId.$dirty) return errors;
+      !this.$v.tokenId.required && errors.push("Token ID is required");
+      return errors;
+    },
+    tokenUriErrors() {
+      const errors = [];
+      if (!this.$v.tokenUri.$dirty) return errors;
+      !this.$v.tokenUri.required && errors.push("Token Uri is required");
+      return errors;
+    },
+    quantityErrors() {
+      const errors = [];
+      if (!this.$v.quantity.$dirty) return errors;
+      !this.$v.quantity.minValue && errors.push("Quantity must be > 0");
+      !this.$v.quantity.required && errors.push("Quantity is required");
+      return errors;
+    },
+    startTimeErrors() {
+      const errors = [];
+      if (!this.$v.startTime.$dirty) return errors;
+      !this.$v.startTime.required && errors.push("Start Time is required");
+      return errors;
+    },
+    endTimeErrors() {
+      const errors = [];
+      if (!this.$v.endTime.$dirty) return errors;
+      !this.$v.endTime.required && errors.push("End Time is required");
+      return errors;
     }
   }
 };
--- src/main.js
+++ src/main.js
@@ -3,8 +3,11 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
+import Vuelidate from 'vuelidate'
+
 
 Vue.config.productionTip = false
+Vue.use(Vuelidate)
 
 new Vue({
   router,
