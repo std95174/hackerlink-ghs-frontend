53e92f1434932210a7f60439439f2777a418be9f
Frank Wang <frankwang95174@gmail.com>
2021-05-22 02:28:52 +0800

    update pinture contract abi and show tokens

--- src/views/PintureMarket.vue
+++ src/views/PintureMarket.vue
@@ -16,33 +16,107 @@
         </v-slide-y-transition>
       </v-col>
     </v-row>
-    <v-row align="center" justify="center">
-      <v-col class="text-right"
-        ><v-btn @click="photoNFTDialog = true"
-          >📷 Make Your Photo NFT</v-btn
-        ></v-col
-      >
-      <v-col class="text-left"
-        ><v-btn @click="licenseNFTDialog = true"
-          >Make Your License NFT 🔐</v-btn
-        ></v-col
-      >
-    </v-row>
-    <v-row class="mr-6 ml-6 pa-6">
-      <v-col>
-        <span> </span>
-      </v-col>
+    <v-row align="center" justify="center" class="mb-3">
+      <v-data-table :headers="headers" :items="pintures">
+        <template v-slot:[`item.photo`]="{ item }">
+          <v-container>
+            <v-row align="center" justify="center" class="pa-3">
+              <v-flex xs4>
+                <v-layout justify-center align-center>
+                  <v-img
+                    width="250"
+                    :src="`http://ipfs.infura.io:5001/api/v0/cat?arg=${item.photo}`"
+                  >
+                  </v-img>
+                </v-layout>
+              </v-flex>
+            </v-row>
+          </v-container>
+        </template>
+        <template v-slot:[`item.actions`]="{}">
+          <v-btn color="primary" @click="alert('this feature is on the way!')">
+            Buy
+          </v-btn>
+        </template>
+      </v-data-table>
     </v-row>
   </v-parallax>
 </template>
   
 <script>
+import { ethers } from "ethers";
+import pintureJson from "../assets/contracts/Pinture.json";
+import licenseTokenJson from "../assets/contracts/LicenseToken.json";
 export default {
   name: "PintureMarket",
   data() {
     return {
-      show: false
+      show: false,
+      headers: [
+        // { text: "Token ID", align: "center", value: "tokenId", width: "10%" },
+        { text: "Token URI", align: "center", value: "tokenUri", width: "20%" },
+        { text: "Photo", align: "center", value: "photo", width: "40%" },
+        { text: "Price", align: "center", value: "price", width: "10%" },
+        {
+          text: "Actions",
+          align: "center",
+          value: "actions",
+          sortable: false,
+          width: "20%"
+        }
+      ],
+      pintures: []
     };
+  },
+  methods: {
+    alert() {
+      alert("this feature is on the way!");
+    }
+  },
+  async mounted() {
+    const ethersJsProvider = new ethers.providers.Web3Provider(window.ethereum);
+
+    const contractAddress = process.env.VUE_APP_PINTURE_CONTRACT_ADDRESS;
+    const abi = pintureJson.abi;
+
+    // The Contract object
+    const pintureContract = new ethers.Contract(
+      contractAddress,
+      abi,
+      ethersJsProvider
+    );
+    const pintureWithSigner = pintureContract.connect(
+      ethersJsProvider.getSigner()
+    );
+
+    const licenseTokenContract = new ethers.Contract(
+      process.env.VUE_APP_LICENSE_CONTRACT_ADDRESS,
+      licenseTokenJson.abi,
+      ethersJsProvider
+    );
+    const licenseTokenWithSigner = licenseTokenContract.connect(
+      ethersJsProvider.getSigner()
+    );
+
+    if (this.currentAccount == "") {
+      alert("connect metamask first");
+      return;
+    }
+
+    const tokens = await pintureWithSigner.getListedTokens();
+    const vm = this;
+    for (let i = 0; i < tokens.length; i++) {
+      pintureWithSigner.getPrice(tokens[i]).then((price) => {
+        licenseTokenWithSigner.tokenURI(tokens[i]).then((tokenUri) => {
+          const cid = tokenUri.split("ipfs://");
+          vm.pintures.push({
+            price: price,
+            tokenUri: tokenUri,
+            photo: cid[1]
+          });
+        });
+      });
+    }
   },
   created() {
     const vm = this;
