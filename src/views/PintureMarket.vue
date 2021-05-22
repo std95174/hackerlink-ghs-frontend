0a26d717af8a3b654a37abe61f0d5455ebbb40c3
Frank Wang <frankwang95174@gmail.com>
2021-05-22 21:02:25 +0800

    bug fixed

--- src/views/PintureMarket.vue
+++ src/views/PintureMarket.vue
@@ -17,44 +17,114 @@
       </v-col>
     </v-row>
     <v-row align="center" justify="center" class="mb-3 mr-3 ml-3">
-      <v-data-table
-        :headers="headers"
-        :items="pintures"
-        no-data-text="trying to get licenses..."
-        :loading="loading"
-        dark
-      >
-        <template v-slot:[`item.photo`]="{ item }">
-          <v-container>
-            <v-row align="center" justify="center" class="pa-3">
-              <v-flex xs4>
-                <v-layout justify-center align-center>
-                  <v-img
-                    width="350"
-                    :src="`http://ipfs.infura.io:5001/api/v0/cat?arg=${item.photo}`"
-                  >
-                  </v-img>
-                </v-layout>
-              </v-flex>
-            </v-row>
-          </v-container>
-        </template>
+      <v-card width="75vw">
+        <v-toolbar dark flat>
+          <v-toolbar-title>{{ tabItems[tab] }}</v-toolbar-title>
 
-        <template v-slot:[`item.price`]="{ item }">
-          {{ item.price }} ETH
-        </template>
-        <template v-slot:[`item.actions`]="{ item }">
-          <v-btn
-            color="primary"
-            @click="buy(item.tokenId, item.price)"
-            outlined
-            :disabled="txLoading"
-            :loading="txLoading"
-          >
-            Buy
-          </v-btn>
-        </template>
-      </v-data-table>
+          <v-spacer></v-spacer>
+          <template v-slot:extension>
+            <v-tabs v-model="tab" align-with-title>
+              <v-tabs-slider color="white"></v-tabs-slider>
+
+              <!-- <v-tab v-for="item in tabItems" :key="item">
+                {{ item }}
+              </v-tab> -->
+              <v-tab>Market</v-tab>
+              <v-tab @click="getLicenseTokens">My Licenses</v-tab>
+            </v-tabs>
+          </template>
+        </v-toolbar>
+
+        <v-tabs-items v-model="tab">
+          <v-tab-item>
+            <v-data-table
+              :headers="headers"
+              :items="pintures"
+              :no-data-text="
+                pintures.length == 0 && loading
+                  ? 'Trying to get licenses...'
+                  : 'There\'s no license on sale now'
+              "
+              :loading="loading"
+              dark
+            >
+              <template v-slot:[`item.photo`]="{ item }">
+                <v-container>
+                  <v-row align="center" justify="center" class="pa-3">
+                    <v-flex xs4>
+                      <v-layout justify-center align-center>
+                        <v-img
+                          width="350"
+                          :src="`http://ipfs.infura.io:5001/api/v0/cat?arg=${item.photo}`"
+                        >
+                        </v-img>
+                      </v-layout>
+                    </v-flex>
+                  </v-row>
+                </v-container>
+              </template>
+
+              <template v-slot:[`item.price`]="{ item }">
+                {{ item.price }} ETH
+              </template>
+              <template v-slot:[`item.actions`]="{ item }">
+                <v-btn
+                  color="primary"
+                  @click="buy(item.tokenId, item.price)"
+                  outlined
+                  :disabled="txLoading"
+                  :loading="txLoading"
+                >
+                  Buy
+                </v-btn>
+              </template>
+            </v-data-table>
+          </v-tab-item>
+
+          <v-tab-item>
+            <v-data-table
+              :headers="licenseHeaders"
+              :items="licenseTokens"
+              :loading="tableLoading"
+              :no-data-text="
+                licenseTokens.length == 0 && tableLoading
+                  ? 'Trying to get licenses...'
+                  : 'You don\'t have any license yet, take a photo a make your own NFT!'
+              "
+              dark
+            >
+              <template v-slot:[`item.photo`]="{ item }">
+                <v-container>
+                  <v-row align="center" justify="center" class="pa-3">
+                    <v-flex xs4>
+                      <v-layout justify-center align-center>
+                        <v-img
+                          width="250"
+                          :src="`http://ipfs.infura.io:5001/api/v0/cat?arg=${item.photo}`"
+                        >
+                        </v-img>
+                      </v-layout>
+                    </v-flex>
+                  </v-row>
+                </v-container>
+              </template>
+              <template v-slot:[`item.actions`]="{ item }">
+                <v-btn
+                  color="primary"
+                  :disabled="loading"
+                  :loading="txLoading"
+                  @click="cancel(item.tokenId)"
+                  outlined
+                  v-if="item.isOnSale"
+                >
+                  Stop Selling
+                </v-btn>
+                <span v-else>Not for Sale</span>
+              </template>
+            </v-data-table>
+          </v-tab-item>
+        </v-tabs-items>
+      </v-card>
     </v-row>
   </v-parallax>
 </template>
@@ -69,6 +139,8 @@ export default {
   data() {
     return {
       show: false,
+      tab: "tab",
+      tabItems: ["Market", "My Licenses"],
       headers: [
         // { text: "Token ID", align: "center", value: "tokenId", width: "10%" },
         // { text: "Token URI", align: "center", value: "tokenUri" },
@@ -85,7 +157,24 @@ export default {
         }
       ],
       pintures: [],
+
+      licenseHeaders: [
+        // { text: "Token ID", align: "center", value: "tokenId", width: "10%" },
+        // { text: "Token URI", align: "center", value: "tokenUri" },
+        { text: "Photo", align: "center", value: "photo" },
+        { text: "Quantity", align: "center", value: "quantity" },
+        { text: "Start", align: "center", value: "startTime" },
+        { text: "End", align: "center", value: "endTime" },
+        {
+          text: "Actions",
+          align: "center",
+          value: "actions",
+          sortable: false
+        }
+      ],
+      licenseTokens: [],
       loading: false,
+      tableLoading: false,
       txLoading: false
     };
   },
@@ -97,12 +186,12 @@ export default {
         return;
       }
       try {
-        this.loading = true;
+        this.txLoading = true;
         pintureWithSigner.buy(tokenId, {
           value: ethers.utils.parseEther(price)
         });
       } catch (error) {
-        this.loading = false;
+        this.txLoading = false;
         console.log(error);
         alert("something error");
         return;
@@ -111,8 +200,10 @@ export default {
       try {
         const vm = this;
         pintureWithSigner.on("Buy", (licenseTokenId, buyer, price, event) => {
-          vm.loading = false;
+          vm.txLoading = false;
           console.log(event);
+          this.getPintures();
+          this.getLicenseTokens();
           alert(
             `Congratulations!! You've bount a License, ID: ${licenseTokenId}`
           );
@@ -120,37 +211,123 @@ export default {
       } catch (error) {
         console.log(error);
       }
-    }
-  },
-  async mounted() {
-    this.loading = true;
-    const tokens = await pintureWithSigner.getListedTokens();
-    const vm = this;
-    for (let i = 0; i < tokens.length; i++) {
-      pintureWithSigner.getPrice(tokens[i]).then((price) => {
-        licenseTokenWithSigner
-          .getLicenseTokenInfo(tokens[i])
-          .then((licenseTokenInfo) => {
-            licenseTokenWithSigner.tokenURI(tokens[i]).then((tokenUri) => {
-              const cid = tokenUri.split("ipfs://");
-              vm.pintures.push({
-                tokenId: tokens[i],
-                price: ethers.utils.formatEther(price),
-                tokenUri: tokenUri,
-                photo: cid[1],
-                quantity: licenseTokenInfo[1],
-                startTime: new Date(licenseTokenInfo[2].toNumber())
-                  .toISOString()
-                  .substr(0, 10),
-                endTime: new Date(licenseTokenInfo[3].toNumber())
-                  .toISOString()
-                  .substr(0, 10)
+    },
+    async getPintures() {
+      this.pintures.length = 0;
+      this.loading = true;
+      const tokens = await pintureWithSigner.getListedTokens();
+      const vm = this;
+      for (let i = 0; i < tokens.length; i++) {
+        pintureWithSigner.getPrice(tokens[i]).then((price) => {
+          licenseTokenWithSigner
+            .getLicenseTokenInfo(tokens[i])
+            .then((licenseTokenInfo) => {
+              licenseTokenWithSigner.tokenURI(tokens[i]).then((tokenUri) => {
+                const cid = tokenUri.split("ipfs://");
+                vm.pintures.push({
+                  tokenId: tokens[i],
+                  price: ethers.utils.formatEther(price),
+                  tokenUri: tokenUri,
+                  photo: cid[1],
+                  quantity: licenseTokenInfo[1],
+                  startTime: new Date(licenseTokenInfo[2].toNumber())
+                    .toISOString()
+                    .substr(0, 10),
+                  endTime: new Date(licenseTokenInfo[3].toNumber())
+                    .toISOString()
+                    .substr(0, 10)
+                });
               });
             });
+        });
+      }
+      this.loading = false;
+    },
+    async getLicenseTokens() {
+      this.licenseTokens.length = 0;
+      if (this.currentAccount == "") {
+        alert("connect metamask first");
+        this.tab = 0;
+        return;
+      }
+      const balance = await licenseTokenWithSigner.balanceOf(
+        this.currentAccount
+      );
+      if (balance == 0) {
+        alert("sorry, you don't have any licenses");
+        this.tableLoading = false;
+        return;
+      }
+      const vm = this;
+      this.tableLoading = true;
+      console.log(this.currentAccount);
+
+      for (let index = 0; index < balance; index++) {
+        licenseTokenWithSigner
+          .tokenOfOwnerByIndex(this.currentAccount, index)
+          .then((tokenId) => {
+            licenseTokenWithSigner.tokenURI(tokenId).then((tokenUri) => {
+              licenseTokenWithSigner
+                .getLicenseTokenInfo(tokenId)
+                .then((licenseTokenInfo) => {
+                  {
+                    pintureWithSigner.getPrice(tokenId).then((price) => {
+                      const cid = tokenUri.split("ipfs://");
+                      vm.licenseTokens.push({
+                        tokenId: tokenId,
+                        tokenUri: tokenUri,
+                        photo: cid[1],
+                        quantity: licenseTokenInfo[1],
+                        startTime: new Date(licenseTokenInfo[2].toNumber())
+                          .toISOString()
+                          .substr(0, 10),
+                        endTime: new Date(licenseTokenInfo[3].toNumber())
+                          .toISOString()
+                          .substr(0, 10),
+                        isOnSale:
+                          ethers.utils.formatEther(price) == 0 ? false : true
+                      });
+                    });
+                  }
+                });
+            });
+            if (index == balance - 1) {
+              vm.tableLoading = false;
+            }
           });
-      });
+      }
+    },
+    cancel(tokenId) {
+      if (this.currentAccount == "") {
+        alert("connect metamask first");
+        return;
+      }
+      try {
+        this.txLoading = true;
+        pintureWithSigner.cancel(tokenId);
+      } catch (error) {
+        this.txLoading = false;
+        console.log(error);
+        alert("something error");
+        return;
+      }
+
+      try {
+        const vm = this;
+        pintureWithSigner.on("Cancel", (licenseTokenId, event) => {
+          vm.txLoading = false;
+          console.log(event);
+          this.getPintures();
+          alert(`Your license had removed from market`);
+        });
+      } catch (error) {
+        console.log(error);
+      }
     }
-    this.loading = false;
+  },
+
+  async mounted() {
+    await this.getPintures();
   },
   created() {
     const vm = this;
